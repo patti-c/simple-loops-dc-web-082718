f33617fc329bf0797d0837d0246228fcff2ff699
Patricia Carter <patricia4194@gmail.com>
2018-08-06 22:06:35 +0000

    automatically backed up by learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -11,7 +11,7 @@ end
 
 def times_iterator(number_of_times)
   phrase = "Welcome to Flatiron School's Web Development Course!"
-  number_of_times.times puts phrase
+  number_of_times.times do puts phrase
 end
 
 def while_iterator(number_of_times)
